d8915435f117fe0388e457388646cf779187e199
godwinn22 <godwinohazulike@gmail.com>
2024-10-22 15:35:11 +0100

    Created serializers for the Doctor model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,32 +1,46 @@
-from .models import Patient
+from .models import Patient, Doctor, Appointment
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
-class PatientSerializer(serializers.ModelSerializer):
+
+class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Patient
-        fields = ['id', 'name', 'email', 'phone_number', 'password', 'role', 'age', 'gender']
+        model = Doctor
+        fields = ['id', 'name', 'email', 'phone_number', 'role', 'specialty', 'bio', 'password']
         extra_kwargs = {'password': {'write_only': True}}
-    
+
     def create(self, validated_data):
-        patient = Patient(
+        doctor = Doctor(
             email=validated_data['email'],
             name=validated_data['name'],
             phone_number=validated_data['phone_number'],
-            age=validated_data['age'],
-            gender=validated_data['gender'],
+            specialty=validated_data['specialty'],
+            bio=validated_data['bio'],
         )
-        patient.set_password(validated_data['password'])  # Hash the password
-        patient.save()
-        return patient
-    
+        doctor.set_password(validated_data['password'])
+        doctor.save()
+        return doctor
+
+
+class DoctorLoginSerializer(serializers.Serializer):
+    """Doctor Login Serializer"""
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        doctor = authenticate(email=email, password=password)
+
+        if doctor and doctor.is_active:
+            return doctor
+        raise serializers.ValidationError("Invalid credentials or account is inactive.")
     def update(self, instance, validated_data):
-        # Update the patient instance with the validated data
+        # Update the doctor instance with the validated data
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         return instance
-    
-class LoginSerializer(serializers.Serializer):
-    """Serializer for the Login View"""
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(required=True)
+
+
